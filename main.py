5a230107ac2bf445d49ff465b242c2e4502f43ee
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2017-07-25 16:04:48 -0400

    Fixed mistakes

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import webapp2
 import jinja2
 import logging
 import urllib
+import os
 import re
 from google.appengine.ext import ndb
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
@@ -43,62 +44,63 @@ class MainHandler(webapp2.RequestHandler):
 
 class ViewOrientation(webapp2.RequestHandler):
     def get(self):
-        try:
-            leader_id = self.request.get("leader_id")
-            orientation = Orientation.query(Orientation.leader.email_address == leader_id).get()
-            if not orientation:
-                raise Exception()
-            else:
-                leader_info = {
-                    "first_name" : orientation.leader.first_name,
-                    "last_name" : orientation.leader.last_name,
-                    "email_address" : orientation.leader.email_address,
-                }
-                members = orientation.followers
-                orientation_members = []
-                if members:
-                    cornellians = Cornellian.query(Cornellian.email_address.IN(members)).fetch()
-                    for cornellian in cornellians:
-                        class_members.append({
-                            "first_name"    : cornellian.first_name,
-                            "last_name"     : cornellian.last_name,
-                            "email_address" : cornellian.email_address,
-                        })
-                self.response.out.write(JINJA_ENVIRONMENT.get_template("view_orientation.html").render({"orientation_members" : orientation_members}))
-        except Exception as e:
-            logging.error(e)
-            self.response.out.write(JINJA_ENVIRONMENT.get_template("view_orientation_error.html").render())
+        leader_id = self.request.get("leader_id")
+        orientation = Orientation.query(Orientation.leader.email_address == leader_id).get()
+        if not orientation:
+            raise Exception()
+        else:
+            leader_info = {
+                "first_name" : orientation.leader.first_name,
+                "last_name" : orientation.leader.last_name,
+                "email_address" : orientation.leader.email_address,
+            }
+            members = [follower.email_address for follower in orientation.followers]
+            orientation_members = []
+            if members:
+                cornellians = Cornellian.query(Cornellian.email_address.IN(members)).fetch()
+                for cornellian in cornellians:
+                    orientation_members.append({
+                        "first_name"    : cornellian.first_name,
+                        "last_name"     : cornellian.last_name,
+                        "email_address" : cornellian.email_address,
+                    })
+            self.response.out.write(JINJA_ENVIRONMENT.get_template("view_orientation.html").render({"leader_info" : leader_info, "orientation_members" : orientation_members}))
+        # except Exception as e:
+        #     logging.error(e)
+        #     self.response.out.write(JINJA_ENVIRONMENT.get_template("view_orientation_error.html").render())
 
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
-        message = str(mail_message.original).splitlines()
-        froms = []
-        for line in message:
-            if line[0:5] == "From:":
-                froms.append(line)
+        message = mail_message.bodies('text/plain')
+        for content_type, body in message:
+            decoded_message = body.decode().splitlines()
+            froms = []
+            for line in decoded_message:
+                if line[0:5] == "From:" or line[0:3] == "To:":
+                    froms.append(line)
 
-        people = []
-        for person in froms:
-            splitperson = person.split()
-            email_address = re.search(r'[\w\.-]+@cornell.edu+', splitperson[2]).group(0)
-            if email_address == "":
-                continue
+            people = []
+            for person in froms:
+                splitperson = person.split()[1:]
+                email_address = splitperson[2][1:-1]
+                if email_address == "":
+                    continue
 
-            newperson = Cornellian()
-            newperson.first_name = splitperson[0]
-            newperson.last_name = splitperson[1]
-            newperson.email_address = email_address
+                newperson = Cornellian()
+                newperson.first_name = splitperson[0]
+                newperson.last_name = splitperson[1]
+                newperson.email_address = email_address
 
-            people.append(newperson)
+                people.append(newperson)
 
-        ndb.put_multi(people)
+            ndb.put_multi(people)
 
-        orientation = Orientation.query(Orientation.leader.email_address == people[1].email_address).fetch()
-        if orientation == [] or orientation == None:
-            orientation = Orientation()
-            orientation.leader = people[1]
-            orientation.followers.append(people[0])
-            orientation.put()
+            orientation = Orientation.query(Orientation.leader.email_address == people[0].email_address).fetch()
+            if orientation == [] or orientation == None:
+                orientation = Orientation()
+                orientation.leader = people[0]
+                orientation.followers.append(people[1])
+                orientation.put()
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
